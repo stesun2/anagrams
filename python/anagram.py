4ce227f73eea7268e4fad32a645312ea50b76517
Christina Angeline <christina.kim.angeline@gmail.com>
2019-06-13 23:26:02 -0500

    updated file and function name

--- python/anagram.py
+++
@@ -1,4 +0,0 @@
-# Don't forget to run your tests!
-
-def is_anagram(string1, string2):
-		# Your code here
---
+++ python/character_match.py
@@ -0,0 +1,4 @@
+# Don't forget to run your tests!
+
+def is_character_match(string1, string2):
+		# Your code here
