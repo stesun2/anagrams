74d513c2521cf3f137c5ae96282531d639a721b2
stesun2 <ss27@illinois.edu>
2021-05-27 23:03:25 -0500

    anagram started, work in progress

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,19 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
+
+	new_string1 = string1.lower()
+	new_string2 = string2.lower()
+
+	print(new_string1)
+	print(new_string2)
+
+
 	pass
 	# Your code here
+
+
+print(is_character_match('HELLO', 'elloh'))
+print(is_character_match('bonjour', 'bonjovi'))
+print(is_character_match('Cao', 'OaC'))
+print(is_character_match('123abc', '321cba'))
